aae09e478673d74041018acc32538703b24f258b
unknown <whykatherine@gmail.com>
2019-08-16 15:53:03 +0800

    style: clean up code

--- data/blend.glsl
+++ data/blend.glsl
@@ -12,17 +12,22 @@ uniform sampler2D tex_serif;
 varying vec4 vertColor;
 varying vec4 vertTexCoord;
 
-float camTint = 0.5;
+// float camTint = 0.5;
 float maskThresh = 0.6;
 
 void main() {
-  vec4 cam = texture2D(tex_cam, vertTexCoord.st);
-  cam = vec4(cam.rgb, camTint); // tint cam
-  vec4 fluid = texture2D(tex_fluid, vec2(vertTexCoord.s, 1.0 - vertTexCoord.t)); // invert y
-  vec4 mask = step(maskThresh, texture2D(tex_fluid, vertTexCoord.st)); // apply bw threshold
-  vec4 sans = texture2D(tex_sans, vertTexCoord.st);
+  // coordinates
+  vec2 texCoord = vertTexCoord.st;
+  vec2 invTexCoord = vec2(vertTexCoord.s, 1.0 - vertTexCoord.t); // invert y
+  // textures
+  // vec4 cam = texture2D(tex_cam, texCoord);
+  // cam = vec4(cam.rgb, camTint); // tint cam
+  vec4 fluid = texture2D(tex_fluid, invTexCoord);
+  vec4 mask = step(maskThresh, texture2D(tex_fluid, invTexCoord)); // apply bw threshold
+  vec4 sans = texture2D(tex_sans, texCoord);
+  vec4 serif = texture2D(tex_serif, texCoord);
+  // blend
   sans = step((1.0 - mask.a), sans); // inverted mask
-  vec4 serif = texture2D(tex_serif, vertTexCoord.st);
   serif = step(mask.a, serif); // mask
   vec4 color = fluid + sans * serif;
   gl_FragColor = color;
