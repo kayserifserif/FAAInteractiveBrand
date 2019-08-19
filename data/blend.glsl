2d7661b95314a47991a7f98729f77959a5c72eac
unknown <whykatherine@gmail.com>
2019-08-19 14:50:59 +0800

    feat: update glsl version from 120 to 330

--- data/blend.glsl
+++ data/blend.glsl
@@ -1,3 +1,5 @@
+#version 330
+
 #ifdef GL_ES
 precision mediump float;
 precision mediump int;
@@ -8,8 +10,10 @@ uniform sampler2D tex_fluid;
 uniform sampler2D tex_sans;
 uniform sampler2D tex_serif;
 
-varying vec4 vertColor;
-varying vec4 vertTexCoord;
+in vec4 vertColor;
+in vec4 vertTexCoord;
+
+out vec4 fragColor;
 
 // float camTint = 0.5;
 float maskThresh = 0.6;
@@ -19,15 +23,15 @@ void main() {
   vec2 texCoord = vertTexCoord.st;
   vec2 invTexCoord = vec2(vertTexCoord.s, 1.0 - vertTexCoord.t); // invert y
   // textures
-  // vec4 cam = texture2D(tex_cam, texCoord);
+  // vec4 cam = texture(tex_cam, texCoord);
   // cam = vec4(cam.rgb, camTint); // tint cam
-  vec4 fluid = texture2D(tex_fluid, invTexCoord);
-  vec4 mask = step(maskThresh, texture2D(tex_fluid, invTexCoord)); // apply bw threshold
-  vec4 sans = texture2D(tex_sans, texCoord);
-  vec4 serif = texture2D(tex_serif, texCoord);
+  vec4 fluid = texture(tex_fluid, invTexCoord);
+  vec4 mask = step(maskThresh, texture(tex_fluid, invTexCoord)); // apply bw threshold
+  vec4 sans = texture(tex_sans, texCoord);
+  vec4 serif = texture(tex_serif, texCoord);
   // blend
   sans = step(1.0 - mask.a, sans); // inverted mask
   serif = step(mask.a, serif); // mask
   vec4 color = fluid + sans * serif;
-  gl_FragColor = color;
+  fragColor = color;
 }
